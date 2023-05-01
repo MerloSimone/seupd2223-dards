f3677873a72a9b69d96316535736701864dc79f6
MerloSimone <simone.merlo@studenti.unipd.it>
2023-05-01 17:23:56 +0200

    Added a class to make our own translation from french.

---
+++ code/src/main/java/it/unipd/dei/dards/TranslationTestClass.java
@@ -0,0 +1,35 @@
+package it.unipd.dei.dards;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class TranslationTestClass {
+
+    public static void main(String[] args) throws IOException {
+        String text = "piquet de terre";
+        System.out.println("Translated text: " + translate("fr", "en", text));
+    }
+
+    private static String translate(String langFrom, String langTo, String text) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbzq2kAPMVypjdniez1vojiBbbxxGjtTr_CeXNNahqr9MnbH-qM0v94HOxH9wCr15M-5/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+}
