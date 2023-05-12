3888e3d284155f8593b25e2c2b577591796d1ea4
MerloSimone <simone.merlo@studenti.unipd.it>
2023-05-12 22:05:33 +0200

    added BM25TRANSLATEDQUERIES system to master branch

---
+++ code/BM25TRANSLATEDQUERIES/src/main/java/it/unipd/dei/dards/HelloEnglish.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2021-2022 University of Padua, Italy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.unipd.dei.dards;
+
+
+import it.unipd.dei.dards.index.DirectoryIndexer;
+import it.unipd.dei.dards.parse.LongEvalParser;
+import it.unipd.dei.dards.search.Searcher;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.StopFilterFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.en.KStemFilterFactory;
+import org.apache.lucene.analysis.pattern.PatternReplaceFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.apache.lucene.search.similarities.*;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Introductory example on how to use <a href="https://lucene.apache.org/" target="_blank">Apache Lucene</a> to index
+ * and search the LongEval corpus.
+ *
+ * @author DARDS
+ * @version 1.0
+ * @since 1.0
+ */
+public class HelloEnglish {
+
+    /**
+     * Main method of the class.
+     *
+     * @param args command line arguments. If provided, {@code args[0]} contains the path the the index directory;
+     *             {@code args[1]} contains the path to the run file.
+     * @throws Exception if something goes wrong while indexing and searching.
+     */
+    public static void main(String[] args) throws Exception {
+
+        final int ramBuffer = 256;
+        final String docsPath = "./input/English/Documents/Trec";
+        final String indexPath = "code/experiment/index-stop-stem";
+
+        final String extension = "txt";
+        final int expectedDocs = 1570734;
+        final String charsetName = StandardCharsets.UTF_8.name();
+
+        final Analyzer a_docs = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class)
+                .addTokenFilter(LowerCaseFilterFactory.class)
+                .addTokenFilter(PatternReplaceFilterFactory.NAME, "pattern", "[\\p{Punct}&&[^-]]")
+                .addTokenFilter(StopFilterFactory.class)
+                //.addTokenFilter(OpenNLPLemmatizerFilterFactory.NAME, "lemmatizerModel", "en-lemmatizer.bin")
+                .addTokenFilter(KStemFilterFactory.class)
+                //.addTokenFilter(NGramFilterFactory.NAME, "minGramSize", "3", "maxGramSize", "10")
+                .build();
+
+        final Analyzer a_query = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class)
+                .addTokenFilter(LowerCaseFilterFactory.class)
+                .addTokenFilter(PatternReplaceFilterFactory.NAME, "pattern", "[\\p{Punct}&&[^-]]")
+                .addTokenFilter(StopFilterFactory.class)
+                //.addTokenFilter(NGramFilterFactory.NAME, "minGramSize", "3", "maxGramSize", "10")
+                //.addTokenFilter(SynonymGraphFilterFactory.NAME, "synonyms", "synonyms_en.txt")
+                //.addTokenFilter(FlattenGraphFilterFactory.class)  //SynonymGraphFilter must be followed by FlattenGraphFilter
+                //.addTokenFilter(OpenNLPLemmatizerFilterFactory.NAME, "lemmatizerModel", "en-lemmatizer.bin")
+                .addTokenFilter(KStemFilterFactory.class)
+                //.addTokenFilter(Word2VecSynonymFilter.NAME, "model", "<model_file>")  //Sease filter based on deeplearning4j
+                .build();
+
+        //final Similarity sim = new MultiSimilarity(new Similarity[]{new BM25Similarity(), new DFRSimilarity(new BasicModelIne(), new AfterEffectL(), new NormalizationH2(0.9F))});
+        final Similarity sim = new BM25Similarity();
+
+        final String topics = "./input/English/Queries/train.tsv";
+
+        final String runPath = "code/experiment";
+
+        final String runID = "seupd2223-dards";
+
+        final int maxDocsRetrieved = 1000;
+
+        final int expectedTopics = 50;
+
+        // indexing
+        final DirectoryIndexer i = new DirectoryIndexer(a_docs, sim, ramBuffer, indexPath, docsPath, extension, charsetName,
+                                                        expectedDocs, LongEvalParser.class);
+        i.index();
+
+        // searching
+        final Searcher s = new Searcher(a_query, sim, indexPath, topics, expectedTopics, runID, runPath, maxDocsRetrieved);
+        s.search();
+    }
+
+}
---
+++ code/BM25TRANSLATEDQUERIES/src/main/java/it/unipd/dei/dards/search/Searcher.java
@@ -0,0 +1,392 @@
+/*
+ *  Copyright 2021-2022 University of Padua, Italy
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package it.unipd.dei.dards.search;
+
+import it.unipd.dei.dards.parse.ParsedDocument;
+import it.unipd.dei.dards.utils.TranslationScript;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.StopFilterFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.en.KStemFilterFactory;
+import org.apache.lucene.analysis.en.PorterStemFilterFactory;
+import org.apache.lucene.analysis.pattern.PatternReplaceFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.apache.lucene.benchmark.quality.QualityQuery;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.QueryParserBase;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.*;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import com.univocity.parsers.tsv.TsvParser;
+import com.univocity.parsers.tsv.TsvParserSettings;
+
+/**
+ * Searches a document collection.
+ *
+ * @author DARDS
+ * @version 1.00
+ * @since 1.00
+ */
+public class Searcher {
+
+    /**
+     * The fields of the LongEval topics.
+     *
+     * @author DARDS
+     * @version 1.00
+     * @since 1.00
+     */
+    private static final class TOPIC_FIELDS {
+
+        /**
+         * The title of a topic.
+         */
+        public static final String TITLE = "title";
+
+    }
+
+
+    /**
+     * The identifier of the run
+     */
+    private final String runID;
+
+    /**
+     * The run to be written
+     */
+    private final PrintWriter run;
+
+    /**
+     * The index reader
+     */
+    private final IndexReader reader;
+
+    /**
+     * The index searcher.
+     */
+    private final IndexSearcher searcher;
+
+    /**
+     * The topics to be searched
+     */
+    private final QualityQuery[] topics;
+
+    /**
+     * The query parser
+     */
+    private final QueryParser qp;
+
+    /**
+     * The maximum number of documents to retrieve
+     */
+    private final int maxDocsRetrieved;
+
+    /**
+     * The total elapsed time.
+     */
+    private long elapsedTime = Long.MIN_VALUE;
+
+
+    /**
+     * Creates a new searcher.
+     *
+     * @param analyzer         the {@code Analyzer} to be used.
+     * @param similarity       the {@code Similarity} to be used.
+     * @param indexPath        the directory where containing the index to be searched.
+     * @param topicsFile       the file containing the topics to search for.
+     * @param expectedTopics   the total number of topics expected to be searched.
+     * @param runID            the identifier of the run to be created.
+     * @param runPath          the path where to store the run.
+     * @param maxDocsRetrieved the maximum number of documents to be retrieved.
+     * @throws NullPointerException     if any of the parameters is {@code null}.
+     * @throws IllegalArgumentException if any of the parameters assumes invalid values.
+     */
+    public Searcher(final Analyzer analyzer, final Similarity similarity, final String indexPath,
+                    final String topicsFile, final int expectedTopics, final String runID, final String runPath,
+                    final int maxDocsRetrieved) {
+
+        if (analyzer == null) {
+            throw new NullPointerException("Analyzer cannot be null.");
+        }
+
+        if (similarity == null) {
+            throw new NullPointerException("Similarity cannot be null.");
+        }
+
+        if (indexPath == null) {
+            throw new NullPointerException("Index path cannot be null.");
+        }
+
+        if (indexPath.isEmpty()) {
+            throw new IllegalArgumentException("Index path cannot be empty.");
+        }
+
+        final Path indexDir = Paths.get(indexPath);
+        if (!Files.isReadable(indexDir)) {
+            throw new IllegalArgumentException(
+                    String.format("Index directory %s cannot be read.", indexDir.toAbsolutePath().toString()));
+        }
+
+        if (!Files.isDirectory(indexDir)) {
+            throw new IllegalArgumentException(String.format("%s expected to be a directory where to search the index.",
+                                                             indexDir.toAbsolutePath().toString()));
+        }
+
+        try {
+            reader = DirectoryReader.open(FSDirectory.open(indexDir));
+        } catch (IOException e) {
+            throw new IllegalArgumentException(String.format("Unable to create the index reader for directory %s: %s.",
+                                                             indexDir.toAbsolutePath().toString(), e.getMessage()), e);
+        }
+
+        searcher = new IndexSearcher(reader);
+        searcher.setSimilarity(similarity);
+
+        if (topicsFile == null) {
+            throw new NullPointerException("Topics file cannot be null.");
+        }
+
+        if (topicsFile.isEmpty()) {
+            throw new IllegalArgumentException("Topics file cannot be empty.");
+        }
+
+        try {
+            BufferedReader in = Files.newBufferedReader(Paths.get(topicsFile), StandardCharsets.UTF_8);
+
+            //topics = new TrecTopicsReader().readQueries(in);
+            //topics = array QualityQuery[].
+            TsvParserSettings settings = new TsvParserSettings();
+            settings.getFormat().setLineSeparator("\n");
+            TsvParser parser = new TsvParser(settings);
+            List<String[]> allRows = parser.parseAll(new File(topicsFile));
+
+            topics = new QualityQuery[allRows.size()];
+
+            int i = 0;
+            HashMap<String, String> StringMap;
+            System.out.printf("%n#### Parsing queries ####%n");
+            for (String[] row: allRows){
+                String translatedQuery="";
+                StringMap = new HashMap<>();
+                try{
+                    translatedQuery= TranslationScript.translate("fr","en",row[1]); //translating the query
+                }catch (Exception e){
+                    translatedQuery=row[1];
+                }
+                StringMap.put(TOPIC_FIELDS.TITLE, translatedQuery); //adding the translated query
+                topics[i] = new QualityQuery(row[0], StringMap);
+                System.out.printf("%d/%d: %s | %s | %s\n",i+1, expectedTopics, topics[i].getQueryID(), topics[i].getValue(TOPIC_FIELDS.TITLE),row[1]);
+//                System.out.println("id: "+row[0]+" query: "+row[1]+" \n");
+                i++;
+            }
+
+            //for(int j=0; j<topics.length; j++) System.out.println("!! "+topics[j].getQueryID()+" | "+topics[j].getValue(TOPIC_FIELDS.TITLE));
+            //for(QualityQuery t: topics) System.out.println("--"+t.getQueryID()+" | "+t.getValue(TOPIC_FIELDS.TITLE));
+
+            in.close();
+        } catch (IOException e) {
+            throw new IllegalArgumentException(
+                    String.format("Unable to process topic file %s: %s.", topicsFile, e.getMessage()), e);
+        }
+
+        if (expectedTopics <= 0) {
+            throw new IllegalArgumentException(
+                    "The expected number of topics to be searched cannot be less than or equal to zero.");
+        }
+
+        if (topics.length != expectedTopics) {
+            System.out.printf("Expected to search for %s topics; %s topics found instead.", expectedTopics,
+                              topics.length);
+        }
+
+        qp = new QueryParser(ParsedDocument.FIELDS.BODY, analyzer);
+
+        if (runID == null) {
+            throw new NullPointerException("Run identifier cannot be null.");
+        }
+
+        if (runID.isEmpty()) {
+            throw new IllegalArgumentException("Run identifier cannot be empty.");
+        }
+
+        this.runID = runID;
+
+
+        if (runPath == null) {
+            throw new NullPointerException("Run path cannot be null.");
+        }
+
+        if (runPath.isEmpty()) {
+            throw new IllegalArgumentException("Run path cannot be empty.");
+        }
+
+        final Path runDir = Paths.get(runPath);
+        if (!Files.isWritable(runDir)) {
+            throw new IllegalArgumentException(
+                    String.format("Run directory %s cannot be written.", runDir.toAbsolutePath().toString()));
+        }
+
+        if (!Files.isDirectory(runDir)) {
+            throw new IllegalArgumentException(String.format("%s expected to be a directory where to write the run.",
+                                                             runDir.toAbsolutePath().toString()));
+        }
+
+        Path runFile = runDir.resolve(runID + ".txt");
+        try {
+            run = new PrintWriter(Files.newBufferedWriter(runFile, StandardCharsets.UTF_8, StandardOpenOption.CREATE,
+                                                          StandardOpenOption.TRUNCATE_EXISTING,
+                                                          StandardOpenOption.WRITE));
+        } catch (IOException e) {
+            throw new IllegalArgumentException(
+                    String.format("Unable to open run file %s: %s.", runFile.toAbsolutePath(), e.getMessage()), e);
+        }
+
+        if (maxDocsRetrieved <= 0) {
+            throw new IllegalArgumentException(
+                    "The maximum number of documents to be retrieved cannot be less than or equal to zero.");
+        }
+
+        this.maxDocsRetrieved = maxDocsRetrieved;
+    }
+
+    /**
+     * Returns the total elapsed time.
+     *
+     * @return the total elapsed time.
+     */
+    public long getElapsedTime() {
+        return elapsedTime;
+    }
+
+    /**
+     * /** Searches for the specified topics.
+     *
+     * @throws IOException    if something goes wrong while searching.
+     * @throws ParseException if something goes wrong while parsing topics.
+     */
+    public void search() throws IOException, ParseException {
+
+        System.out.printf("%n#### Start searching ####%n");
+
+        // the start time of the searching
+        final long start = System.currentTimeMillis();
+
+        final Set<String> idField = new HashSet<>();
+        idField.add(ParsedDocument.FIELDS.ID);
+
+        BooleanQuery.Builder bq = null;
+        Query q = null;
+        TopDocs docs = null;
+        ScoreDoc[] sd = null;
+        String docID = null;
+
+        try {
+            for (QualityQuery t : topics) {
+
+                System.out.printf(" Searching for topic %s | %s .%n", t.getQueryID(), t.getValue(TOPIC_FIELDS.TITLE));
+
+                bq = new BooleanQuery.Builder();
+
+                bq.add(qp.parse(QueryParserBase.escape(t.getValue(TOPIC_FIELDS.TITLE))), BooleanClause.Occur.SHOULD);
+                //bq.add(qp.parse(QueryParserBase.escape(t.getValue(TOPIC_FIELDS.DESCRIPTION))), BooleanClause.Occur.SHOULD);
+
+                q = bq.build();
+
+                docs = searcher.search(q, maxDocsRetrieved);
+
+                sd = docs.scoreDocs;
+
+                for (int i = 0, n = sd.length; i < n; i++) {
+                    docID = reader.document(sd[i].doc, idField).get(ParsedDocument.FIELDS.ID);
+
+                    run.printf(Locale.ENGLISH, " %s Q0 %s %d %.6f %s%n", t.getQueryID(), docID, i, sd[i].score,
+                               runID);
+                }
+
+                run.flush();
+
+            }
+        } finally {
+            run.close();
+
+            reader.close();
+        }
+
+        elapsedTime = System.currentTimeMillis() - start;
+
+        System.out.printf("%d topic(s) searched in %d seconds.%n", topics.length, elapsedTime / 1000);
+
+        System.out.printf("#### Searching complete ####%n");
+    }
+
+    /**
+     * Main method of the class. Just for testing purposes.
+     *
+     * @param args command line arguments.
+     * @throws Exception if something goes wrong while indexing.
+     */
+    public static void main(String[] args) throws Exception {
+
+        final String topics = "./input/French/Queries/train.tsv";
+
+        final String indexPath = "code/experiment/index-stop-stem";
+
+        final String runPath = "code/experiment";
+
+        final String runID = "seupd2223-dards-transquery";
+
+        final int maxDocsRetrieved = 1000;
+
+        final Analyzer a = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class)
+                .addTokenFilter(LowerCaseFilterFactory.class)
+                .addTokenFilter(PatternReplaceFilterFactory.NAME, "pattern", "[\\p{Punct}&&[^-]]")
+                .addTokenFilter(StopFilterFactory.class)
+                //.addTokenFilter(NGramFilterFactory.NAME, "minGramSize", "3", "maxGramSize", "10")
+                //.addTokenFilter(SynonymGraphFilterFactory.NAME, "synonyms", "synonyms_en.txt")
+                //.addTokenFilter(FlattenGraphFilterFactory.class)  //SynonymGraphFilter must be followed by FlattenGraphFilter
+                //.addTokenFilter(OpenNLPLemmatizerFilterFactory.NAME, "lemmatizerModel", "en-lemmatizer.bin")
+                .addTokenFilter(KStemFilterFactory.class)
+                //.addTokenFilter(Word2VecSynonymFilter.NAME, "model", "<model_file>")  //Sease filter based on deeplearning4j
+                .build();
+        //final Similarity sim = new MultiSimilarity(new Similarity[]{new BM25Similarity(), new DFRSimilarity(new BasicModelIne(), new AfterEffectL(), new NormalizationH2(0.9F))});
+        final Similarity sim = new BM25Similarity();
+
+        Searcher s = new Searcher(a, sim, indexPath, topics, 672, runID, runPath, maxDocsRetrieved);
+
+        s.search();
+
+
+    }
+
+}
